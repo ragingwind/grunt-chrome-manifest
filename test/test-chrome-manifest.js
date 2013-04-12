881182c4f2c9ef5add0d01cf1677ab72e05ec715
ragingwind <ragingwind@gmail.com>
2013-04-13 00:56:48 +0900

    grunt standardized #2

--- test/test-chrome-manifest.js
+++ test/test-chrome-manifest.js
@@ -29,16 +29,16 @@ var directory = function directory(dir) {
   };
 };
 
-var gruntConfigs = function (options) {
-  var configs = {};
-  configs.uglifyName = options.uglify || 'uglify';
-  configs.cssminName = options.cssmin || 'cssmin';
-  return {
-    concat: grunt.config('concat') || {},
-    uglify: grunt.config(configs.uglifyName) || {},
-    cssmin: grunt.config(configs.cssminName) || {}
-  };
-};
+// var gruntConfigs = function (options) {
+//   var configs = {};
+//   configs.uglifyName = options.uglify || 'uglify';
+//   configs.cssminName = options.cssmin || 'cssmin';
+//   return {
+//     concat: grunt.config('concat') || {},
+//     uglify: grunt.config(configs.uglifyName) || {},
+//     cssmin: grunt.config(configs.cssminName) || {}
+//   };
+// };
 
 var _ = grunt.util._;
 
@@ -46,78 +46,81 @@ describe('chrome', function () {
 
   before(directory('temp'));
 
-  var chromeManifest = {
-    options: {
+  var targets = {
+    dist: {
+      options: {
+        background: 'scripts/background.js'
+      },
       src: 'app',
-      dest: 'dist',
-    },
-    buildnumber: {
-      update: true
-    },
-    usemin: {
-      background: 'scripts/background.js'
+      dest: 'dist'
     }
   };
 
+  it('should update the configs and manifest.json', function () {
+    var concat, uglify, cssmin, manifest;
+    var target = targets.dist;
+    var src = target.src;
+    var dest = target.dest;
+    var srcBackground = path.join(src, target.options.background);
+    var destBackground = path.join(dest, target.options.background);
 
-  it('should works with manifest.json', function () {
     grunt.file.copy(path.join(__dirname, 'fixtures/manifest.json'), 'app/manifest.json');
     grunt.log.muted = true;
     grunt.config.init();
-    grunt.config('chromeManifest', chromeManifest);
-    grunt.task.run('chromeManifest:usemin');
+    grunt.config('chromeManifest', {dist:target});
+    grunt.task.run('chromeManifest:dist');
     grunt.task.start();
 
-    var options = chromeManifest.options;
-    var configs = gruntConfigs(options);
-    var manifest = grunt.file.readJSON(path.join(options.src, 'manifest.json'));
+    concat = grunt.config('concat');
+    uglify = grunt.config('uglify');
+    cssmin = grunt.config('cssmin');
+    manifest = grunt.file.readJSON(path.join(src, 'manifest.json'));
 
-    // check concat list
-    _.each(configs.concat.background.src, function (script, i) {
-      assert.equal(configs.concat.background.src[i], path.join(options.src, manifest.background.scripts[i]));
+    // check concat and uglify list on background scripts.
+    _.each(concat.background.src, function (script, i) {
+      assert.equal(concat.background.src[i], path.join(src, manifest.background.scripts[i]));
     });
-    assert.equal(configs.concat.background.dest, path.join(options.dest, chromeManifest.usemin.background));
+    assert.equal(concat.background.dest, destBackground);
+    assert.ok(uglify[destBackground]);
+    assert.equal(uglify[destBackground], destBackground);
 
-    assert.ok(configs.uglify[path.join(options.dest, chromeManifest.usemin.background)]);
-    assert.equal(configs.uglify[path.join(options.dest, chromeManifest.usemin.background)], 'dist/scripts/background.js');
-
+    // check cssmin and uglify list on contents scripts.
     for (var cs = 0, max = manifest.content_scripts.length; cs < max; ++cs) {
-      var file, dest;
-      // check uglify list
-      for (var i = 0, csmax = manifest.content_scripts[cs].js.length; i < csmax; ++i) {
+      var file;
+      for (var i = 0, max = manifest.content_scripts[cs].js.length; i < max; ++i) {
         file = 'scripts/contentscript-' + parseInt(cs, 10) + parseInt(i, 10) + '.js';
-        dest = path.join(options.dest, file);
-        assert.ok(configs.uglify[dest]);
-        assert.equal(configs.uglify[dest], path.join(options.src, file));
+        assert.ok(uglify[path.join(dest, file)]);
+        assert.equal(uglify[path.join(dest, file)], path.join(src, file));
       }
 
-      // check cssmin list
       for (i = 0, max = manifest.content_scripts[cs].css.length; i < max; ++i) {
         file = 'styles/contentstyle-' + parseInt(cs, 10) + parseInt(i, 10) + '.css';
-        dest = path.join(options.dest, file);
-        assert.ok(configs.cssmin[dest]);
-        assert.equal(configs.cssmin[dest], path.join(options.src, file));
+        assert.ok(cssmin[path.join(dest, file)]);
+        assert.equal(cssmin[path.join(dest, file)], path.join(src, file));
       }
     }
 
     // check updated manifest.json
-    manifest = grunt.file.readJSON(path.join(options.dest, 'manifest.json'));
+    manifest = grunt.file.readJSON(path.join(dest, 'manifest.json'));
     assert.ok(manifest);
     assert.ok(manifest.background);
     assert.ok(manifest.background.scripts.length > 0);
-    assert.equal(manifest.background.scripts[0], chromeManifest.usemin.background);
+    assert.equal(manifest.background.scripts[0], target.options.background);
 
   });
 
-  it('should change the buildnumber', function () {
+  it('should update the buildnumber', function () {
+    var target = targets.dist;
+
     grunt.file.copy(path.join(__dirname, 'fixtures/manifest.json'), 'app/manifest.json');
     grunt.log.muted = false;
     grunt.config.init();
-    grunt.config('chromeManifest', chromeManifest);
-    grunt.task.run('chromeManifest:buildnumber');
+    target.options.buildnumber = true;
+    grunt.config('chromeManifest', {dist:target});
+    grunt.task.run('chromeManifest:dist');
     grunt.task.start();
 
-    var manifest = grunt.file.readJSON(path.join(chromeManifest.options.src, 'manifest.json'));
+    var manifest = grunt.file.readJSON(path.join(target.src, 'manifest.json'));
     assert.equal(manifest.version, '0.0.2');
   });
 
